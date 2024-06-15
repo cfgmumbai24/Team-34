2b01d0a199848990e7617060d208e904a9f443a3
Saishree Kouda <96722257+Saishreekouda@users.noreply.github.com>
2024-06-15 23:43:09 +0530

    Update BarChart.jsx

--- frontend/src/components/BarChart.jsx
+++ frontend/src/components/BarChart.jsx
@@ -39,7 +39,7 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+      keys={["quiz1", "quiz2", "quiz3", "quiz4"]}
       indexBy="country"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
